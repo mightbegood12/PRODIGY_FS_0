b46be7fd8011247ddcbecd0c283437288540647f
mightbegood12 <banunaresh1976@gmail.com>
2024-08-16 15:39:52 +0530

    [ADD] Middleware and Custom Error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,16 +1,15 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 
-export const signUP = async (req, res) => {
+export const signUP = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
   try {
     await newUser.save();
     res.status(201).json({ message: "User created Successfully" });
-    console.log(req.body);
-  } catch (err) {
-    res.status(500).json(err.message);
+  } catch (error) {
+    next(error);
   }
 };
 
