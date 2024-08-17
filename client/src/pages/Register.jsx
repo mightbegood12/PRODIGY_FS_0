ace78d466ee5e0fbbb07ea1a6826d9fcbbb0eeba
mightbegood12 <banunaresh1976@gmail.com>
2024-08-17 20:32:08 +0530

    [ADD] Sign-In page Functionality

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Register() {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
-    console.log(formData);
   };
 
   const handleSubmit = async (e) => {
@@ -28,6 +28,7 @@ export default function Register() {
       if (data.success === false) {
         setError(true);
       }
+      navigate("/sign-in");
     } catch (error) {
       setLoading(false);
       setError(true);
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,76 @@
-import React from 'react'
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignIn() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      setLoading(true);
+      setError(false);
+      const res = await fetch("/api/auth/sign-in", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      setLoading(false);
+      if (data.success === false) {
+        setError(true);
+      }
+      navigate("/");
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  };
+
   return (
-    <div>SignIn</div>
-  )
+    <div className="h-screen bg-gradient-to-r from-green-200 to-green-300">
+      <div className="p-3 max-w-lg mx-auto ">
+        <h1 className="text-3xl text-center font-semibold my-7"> Sign In</h1>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+          <input
+            type="email"
+            placeholder="Enter your Email"
+            id="email"
+            className="bg-slate-100 p-3 rounded-lg"
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            id="password"
+            className="bg-slate-100 p-3 rounded-lg"
+            onChange={handleChange}
+          />
+          <button
+            disabled={loading}
+            className="bg-green-400 text-white rounded-lg p-3 uppercase hover:opacity-90 disabled:opacity-50"
+          >
+            {loading ? "Loading..." : "Sign in"}
+          </button>
+        </form>
+        <div className="flex justify-center gap-2 mt-5">
+          <p>Dont have an account?</p>
+          <Link to="/register">
+            <span className="text-blue-500">Sign Up</span>
+          </Link>
+        </div>
+        <p className="text-red-700 mt-5">
+          {error && "User Already Registered!"}
+        </p>
+      </div>
+    </div>
+  );
 }
