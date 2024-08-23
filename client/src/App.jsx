e2fcab183f296678f7b9fee514e16afb57522ea2
mightbegood12 <banunaresh1976@gmail.com>
2024-08-23 21:48:06 +0530

    [Update] Header and Profile

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,26 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "./Pages/Home"
-import About from "./Pages/About"
-import Profile from "./Pages/Profile"
-import Register from "./Pages/Register"
-import SignIn from "./pages/SignIn"
-import Header from "./components/Header"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Profile from "./Pages/Profile";
+import Register from "./Pages/Register";
+import SignIn from "./pages/SignIn";
+import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 
 export default function App() {
   return (
     <BrowserRouter>
-    {/* header*/ }
+      {/* header*/}
       <Header></Header>
       <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/about" element={<About/>}></Route>
-        <Route path="/profile" element={<Profile/>}></Route>
-        <Route path="/register" element={<Register/>}></Route>
-        <Route path="/sign-in" element={<SignIn/>}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />}></Route>
+        </Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/sign-in" element={<SignIn />}></Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
-
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <div className="bg-green-300">
       <div className="font-bold flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -14,8 +16,16 @@ export default function Header() {
           <Link to="/about">
             <li className="font-normal">About</li>
           </Link>
-          <Link to="/sign-in">
-            <li className="font-normal">Sign In</li>
+          <Link to="/profile">
+            {currentUser ? (
+              <img
+                src={currentUser.profilePicture}
+                alt="Profile"
+                className="h-7 w-7 rounded-full object-cover"
+              />
+            ) : (
+              <li className="font-normal">Sign In</li>
+            )}
           </Link>
         </ul>
       </div>
